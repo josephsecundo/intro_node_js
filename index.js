826b01183fb7651bf772b807df33cc6852e6334b
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-02-15 20:41:07 -0300

    AULA 15/02

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+
+let nome = "José";
+
+console.log (nome);
+
+let idade = 34;
+let altura = 1.72;
+
+console.log('nome:  ' + nome + ' idade: ' + idade + ' anos');
+
+
+let trabalha = true;
+
+console.log(trabalha);
+console.log('Trabalha: '+ trabalha === true ? 'Sim' : 'Não');
+
+if (trabalha === true){
+    console.log('Trabalha: Sim')
+}else {
+    console.log('Trabalha: Não');
+}
