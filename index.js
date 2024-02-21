f269af6871aa198f8871b6ca5efbb36e893e0517
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-02-20 22:28:31 -0300

    atualizado comentarios

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 console.log("Hello World");
 
-
+// variavel para inserir nome
 let nome = "José";
 
+// executar nome
 console.log (nome);
 
+// var para iidade e altura
 let idade = 34;
 let altura = 1.72;
 
 console.log('nome:  ' + nome + ' idade: ' + idade + ' anos');
 
 
+// indica se é falsa ou verdadeira
 let trabalha = true;
 
 console.log(trabalha);
@@ -21,3 +24,7 @@ if (trabalha === true){
 }else {
     console.log('Trabalha: Não');
 }
+
+
+//indica se é uma array ou não.
+console.log ((Array.isArray(trabalha)) ? 'Array' : 'Não é Array');
